3c96e8aa5737e94734833f94b0f2b33483c216d2
Roger Santos <roger.santos36@gmail.com>
2022-04-08 15:37:02 -0300

    adicionado contagem total de tasks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,31 @@
 const todosContainer = document.querySelector('[data-todo="todos-container"]')
 const formAdd = document.querySelector('[data-form-add="form-add"]')
 const formSearch = document.querySelector('[data-form-search="form-search"]')
+const totalTasks = document.querySelector('[data-total-tasks="total-tasks"]')
+
+const updateTotalTasks = () => {
+   let tasksAmount = todosContainer.children.length
+   totalTasks.textContent = `${tasksAmount} Tasks`
+}
+
+updateTotalTasks()
+
+formAdd.addEventListener('submit', event => {
+   event.preventDefault()
+
+   const inputValue = event.target.add.value.trim()
+
+   if (inputValue.length) {
+      todosContainer.innerHTML += `
+      <li class="todo__item">
+         <span>${inputValue}</span>
+         <i class="ri-delete-bin-line delete__icon"></i>
+      </li>`
+
+      updateTotalTasks()
+      event.target.reset()
+   }
+})
 
 formSearch.addEventListener('input', event => {
    const inputValue = event.target.value.toLowerCase().trim()
@@ -15,25 +40,11 @@ formSearch.addEventListener('input', event => {
       })
 })
 
-formAdd.addEventListener('submit', event => {
-   event.preventDefault()
-
-   const inputValue = event.target.add.value.trim()
-
-   if (inputValue.length) {
-      todosContainer.innerHTML += `
-      <li class="todo__item">
-         <span>${inputValue}</span>
-         <i class="ri-delete-bin-line delete__icon"></i>
-      </li>`
-      event.target.reset()
-   }
-})
-
 todosContainer.addEventListener('click', event => {
    const clickedElement = event.target
 
    if (Array.from(clickedElement.classList).includes('delete__icon')) {
       clickedElement.parentElement.remove()
+      updateTotalTasks()
    }
 })
